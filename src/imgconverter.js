f3b47cc727e2137a64e569dbe72ba8189fce50ca
Rahugg <rahugg07@gmail.com>
2022-07-23 15:32:30 +0600

    Updated file

--- src/imgconverter.js
+++
@@ -1,9 +0,0 @@
-function encodeImageFileAsURL(element) {
-    var file = element.files[0];
-    var reader = new FileReader();
-    reader.onloadend = function() {
-      console.log('RESULT', reader.result)
-    }
-    reader.readAsDataURL(file);
-  }
-  <input type="file" onchange="encodeImageFileAsURL(this)" />
