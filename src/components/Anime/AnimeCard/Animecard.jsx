5c6a6814b10743c1b6cdf580e8e8e84891b05955
Rahugg <rahugg07@gmail.com>
2022-08-02 03:12:13 +0600

    fixed css

--- src/components/Anime/AnimeCard/Animecard.jsx
+++ src/components/Anime/AnimeCard/Animecard.jsx
@@ -4,22 +4,24 @@ import "./Animecard.css";
 
 const Animecard = ({ anime }) => {
   return (
-    <article className="animelist">
-      <div className="img-container">
-        <img src={anime.img} alt={anime.anime} height="500px" />
+    <div className="containerOfAnimeSection">
+      <div className="animelist">
+        <div className="img-container">
+          <img src={anime.img} alt={anime.anime} height="500px" />
+        </div>
+        <div className="animelist-footer">
+          <h3>Name: {anime.anime}</h3>
+          <h4>Episodes: {anime.episodes}</h4>
+          <p>Genre: {anime.genre}</p>
+          <Link
+            className="btn btn-primary btn-details"
+            to={"/Anime/" + anime.anime}
+          >
+            Details
+          </Link>
+        </div>
       </div>
-      <div className="animelist-footer">
-        <h3>Name: {anime.anime}</h3>
-        <h4>Episodes: {anime.episodes}</h4>
-        <p>Genre: {anime.genre}</p>
-        <Link
-          className="btn btn-primary btn-details"
-          to={"/Anime/" + anime.anime}
-        >
-          Details
-        </Link>
-      </div>
-    </article>
+    </div>
   );
 };
 
--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
+import './Character.css';
 
 function CharacterCard({ anime }) {
   return (
-    <article className="animelist">
+    <article className="mainCharacterList">
       <Card >
         <CardMedia component="img" image={anime.image} alt={anime.author} className = "card__image" />
-        <div className="animelist-footer">
+        <div className="mainCharacterList-footer">
           <h3>Character: {anime.author}</h3>
           <h4>Anime: {anime.anime}</h4>
           <p>Quote: {anime.quote}</p>
