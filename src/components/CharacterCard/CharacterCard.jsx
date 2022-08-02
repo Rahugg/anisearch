bb13f4919e848c6959fa4f8c8071d63b34e9a352
Rahugg <rahugg07@gmail.com>
2022-08-02 15:55:10 +0600

    css done

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
-import './Charactercard.css';
+import "./Charactercard.css";
 
 function CharacterCard({ anime }) {
   return (
     <article className="mainCharacterList">
-      <Card >
-        <CardMedia component="img" image={anime.image} alt={anime.author} className = "card__image" />
+      <Card>
+        <CardMedia component="img" image={anime.image} alt={anime.author} />
         <div className="mainCharacterList-footer">
           <h3>Character: {anime.author}</h3>
           <h4>Anime: {anime.anime}</h4>
--- src/components/SingleAnimeCharacter/SingleAnimeCharacter.jsx
+++ src/components/SingleAnimeCharacter/SingleAnimeCharacter.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 import "./SingleAnimeCharacter.css";
 
 const SingleAnimeCharacter = ({ anime: array }) => {
@@ -8,42 +10,48 @@ const SingleAnimeCharacter = ({ anime: array }) => {
 
   return (
     <>
-      <section className="SingleAnimeCharacter-section">
+      <div className="container__for__button">
         <Link to={"/"} className="btn btn-primary">
           Back Home
         </Link>
-        <div className="SingleAnimeCharacter">
-          <img src={anime.image} alt={anime.author} />
-        </div>
-        <div className="SingleAnimeCharacter-info">
-          <p>
-            <span className="SingleAnimeCharacter-data">Anime:</span>
-            {anime.anime}
-          </p>
-          <p>
-            <span className="SingleAnimeCharacter-data">Character:</span>
-            {anime.author}
-          </p>
-          <p>
-            <span className="SingleAnimeCharacter-data">Age:</span>
-            {anime.age}
-          </p>
-          <p>
-            <span className="SingleAnimeCharacter-data">Episode:</span>
-            {anime.episode}
-          </p>
-          <p>
-            <span className="SingleAnimeCharacter-data"> Birthday:</span>
-            {anime.birthday}
-          </p>
-          <p>
-            <span className="SingleAnimeCharacter-data">Power and skills:</span>
-            <span> {anime.power1}</span>
-            <span> {anime.power2}</span>
-            <span> {anime.power3}</span>
-          </p>
-        </div>
-      </section>
+      </div>
+      <div className="containerForSingleCharacter">
+        <section className="SingleAnimeCharacter-section">
+          <div className="image__character">
+            <CardMedia component="img" image={anime.image} alt={anime.author} />
+          </div>
+          <div className="SingleAnimeCharacter-info">
+            <p>
+              <span className="SingleAnimeCharacter-data">Anime:</span>
+              {anime.anime}
+            </p>
+            <p>
+              <span className="SingleAnimeCharacter-data">Character:</span>
+              {anime.author}
+            </p>
+            <p>
+              <span className="SingleAnimeCharacter-data">Age:</span>
+              {anime.age}
+            </p>
+            <p>
+              <span className="SingleAnimeCharacter-data">Episode:</span>
+              {anime.episode}
+            </p>
+            <p>
+              <span className="SingleAnimeCharacter-data"> Birthday:</span>
+              {anime.birthday}
+            </p>
+            <p>
+              <span className="SingleAnimeCharacter-data">
+                Power and skills:
+              </span>
+              <span> {anime.power1}</span>
+              <span> {anime.power2}</span>
+              <span> {anime.power3}</span>
+            </p>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
